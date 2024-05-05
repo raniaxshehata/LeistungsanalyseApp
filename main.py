6f772cd12b7bb9bc27ac798579c6c8e9356a3534
raniaxshehata <163292776+raniaxshehata@users.noreply.github.com>
2024-05-05 16:58:53 +0200

    Aufgabe 8

--- main.py
+++ main.py
@@ -1,53 +1,26 @@
+import requests
 import json
-from datetime import datetime
-from my_classes import Subject, Supervisor, Experiment  # Annahme: Diese Klassen wurden wie besprochen angelegt
-#build_person und estimate_max_hr Funktionen entfernt( in Klassen integriert) -> nutzt _birthdate um das Alter der Person zu berechnen und daraufhin die maximale Herzrate zu schätzen
 
-def get_experiment_details():
-    experiment_name = input("Enter experiment name: ")
-    date = input("Enter date of experiment (YYYY-MM-DD): ")
+class Person:
+    def __init__(self, first_name):
+        self.first_name = first_name
 
-    supervisor_first_name = input("Enter supervisor's first name: ")
-    supervisor_last_name = input("Enter supervisor's last name: ")
-    supervisor_sex = input("Enter supervisor's sex (male/female): ")
-    supervisor_birthdate = input("Enter supervisor's birthdate (YYYY-MM-DD): ")
-    supervisor = Supervisor(supervisor_first_name, supervisor_last_name, supervisor_sex, supervisor_birthdate)
+    def put(self):
+        url = 'http://example.com/api/person'
+        data = {'first_name': self.first_name}
+        response = requests.post(url, json=data)
+        return response
 
-    subject_first_name = input("Enter subject's first name: ")
-    subject_last_name = input("Enter subject's last name: ")
-    subject_sex = input("Enter subject's sex (male/female): ")
-    subject_birthdate = input("Enter subject's birthdate (YYYY-MM-DD): ")
-    subject = Subject(subject_first_name, subject_last_name, subject_sex, subject_birthdate, experiment_name, date, supervisor)
+class Subject(Person):
+    def __init__(self, first_name, email=None):
+        super().__init__(first_name)
+        self.email = email
 
-    return Experiment(experiment_name, date, supervisor, subject)
+    def update_email(self, new_email):
+        self.email = new_email
+        url = f'http://example.com/api/person/{self.first_name}/update_email'
+        data = {'email': self.email}
+        response = requests.post(url, json=data)
+        return response
 
-# Experiment erstellen, als Datei speichern
-def main():
-    experiment = get_experiment_details()
-    # JSON-Datei
-    with open("experiment.json", "w") as outfile:
-        json.dump({
-            "experiment_name": experiment.experiment_name,
-            "date": experiment.date,
-            "supervisor": {
-                "first_name": experiment.supervisor._first_name,
-                "last_name": experiment.supervisor._last_name,
-                "sex": experiment.supervisor._sex,
-                "birthdate": experiment.supervisor._birthdate
-            },
-            "subject": {
-                "first_name": experiment.subject._first_name,
-                "last_name": experiment.subject._last_name,
-                "sex": experiment.subject._sex,
-                "birthdate": experiment.subject._birthdate,
-                "experiment_name": experiment.subject._experiment_name,
-                "date": experiment.subject._date,
-                "supervisor": {
-                    "first_name": experiment.subject._supervisor._first_name,
-                    "last_name": experiment.subject._supervisor._last_name
-                }
-            }
-        }, outfile, indent=4)
-
-if __name__ == "__main__":
-    main()
+# Weil Supervisor und Experiment vom Ursprungsskript abhängen, ändern wir diese nicht.
